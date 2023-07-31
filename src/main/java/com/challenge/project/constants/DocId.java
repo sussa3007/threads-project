7f58d143c2a0bd6195405ff219834d381c44ee82
sussa3007 <sussa1933@gmail.com>
2023-07-31 18:32:13 +0900

    feat: Threads 요청 DocId Parameter 상수 정의

---
+++ src/main/java/com/challenge/project/constants/DocId.java
@@ -0,0 +1,15 @@
+package com.challenge.project.constants;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum DocId {
+    GET_PROFILE_POST("6232751443445612"),
+    GET_POST("5587632691339264"),
+    GET_LIKED_POST("9360915773983802");
+
+    private final String id;
+
+}
