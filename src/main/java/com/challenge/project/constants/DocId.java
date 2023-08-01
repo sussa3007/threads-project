99aed049d356011541ce80fd050221954a543d2a
sussa3007 <sussa1933@gmail.com>
2023-08-01 15:39:24 +0900

    chore: Profile Replies DocId 상수 추가

--- src/main/java/com/challenge/project/constants/DocId.java
+++ src/main/java/com/challenge/project/constants/DocId.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum DocId {
     GET_PROFILE_POST("6232751443445612"),
     GET_POST("5587632691339264"),
+    GET_PROFILE_REPLIES("6307072669391286"),
     GET_LIKED_POST("9360915773983802");
 
     private final String id;
