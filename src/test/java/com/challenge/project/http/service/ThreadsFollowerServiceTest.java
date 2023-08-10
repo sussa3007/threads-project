1a877d7484f8a346031d98089c124be2b0394a0a
sussa3007 <sussa1933@gmail.com>
2023-08-10 09:59:53 +0900

    test: 임시 테스트 코드 작성하여 정상 데이터 여부 확인

---
+++ src/test/java/com/challenge/project/http/service/ThreadsFollowerServiceTest.java
@@ -0,0 +1,40 @@
+package com.challenge.project.http.service;
+
+import com.challenge.project.http.dto.FollowerResponseDto;
+import com.challenge.project.http.dto.InstagramTokenDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ThreadsFollowerServiceTest {
+
+    @Autowired
+    private ThreadsFollowerService service;
+
+    @Test
+    @DisplayName("")
+    void test() {
+        //Given
+
+        String username = "";
+        String password = "";
+        InstagramTokenDto allFollower = service.findAllFollower(username, password);
+        try {
+            List<FollowerResponseDto> follower = service.findFollower(username, password, allFollower);
+            System.out.println(follower.size());
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        //When
+
+        //Then
+    }
+
+}
