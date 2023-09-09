c453e6b8949ca664716c42f57bed5e6e79410130
sussa3007 <sussa1933@gmail.com>
2023-09-09 15:39:33 +0900

    FIX: 차단됨 요청 거부 Fix 시도 #3

--- src/main/java/com/challenge/project/http/service/ThreadsRankingService.java
+++ src/main/java/com/challenge/project/http/service/ThreadsRankingService.java
@@ -7,7 +7,6 @@ import com.challenge.project.constants.ThreadsRequestProperty;
 import com.challenge.project.exception.ServiceLogicException;
 import com.challenge.project.http.dto.*;
 import com.challenge.project.http.handler.HttpResponseHandler;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
@@ -88,10 +87,10 @@ public class ThreadsRankingService {
 
     public List<String> getThreadsIdListByRapidApi(String userId) throws IOException {
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            HttpGet httpGet = new HttpGet(ThreadsRequestProperty.THREADS_GET_URL_RAPID.getProperty()+userId);
+            HttpGet httpGet = new HttpGet(ThreadsRequestProperty.THREADS_GET_URL_RAPID.getProperty() + userId);
             headerService.setRapidApiDefaultHeader(httpGet, RAPID_API_KEY);
             log.info("Executing request ThreadsId RapidApi = {} ", httpGet.getRequestLine());
-            String  execute = (String) httpclient.execute(httpGet, handler.getDefaultStringHandler());
+            String execute = (String) httpclient.execute(httpGet, handler.getDefaultStringHandler());
             return parsingThreadsIdListByRapidApi(execute);
         } catch (UnrecognizedPropertyException un) {
             throw new ServiceLogicException(ErrorCode.NOT_FOUND);
@@ -101,27 +100,27 @@ public class ThreadsRankingService {
     }
 
 
-
     public List<FindUserDto> getReplyUser(List<String> postList, String username) throws IOException, InterruptedException {
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            String token = getLsdToken();
             List<FindUserDto> userList = new ArrayList<>();
             // 병렬 스트림 처리
-            postList.stream().parallel().forEach( postId -> {
-                log.info("Call Post Reply User List = {}", postId);
-                HttpPost httpPost = new HttpPost(ThreadsRequestProperty.BASE_URL.getProperty());
-                headerService.setThreadsRequestDefaultHeader(httpPost, token, null);
-                UrlEncodedFormEntity entity = getUrlEncodedBody("postID", postId, token, DocId.GET_POST.getId());
-                httpPost.setEntity(entity);
-                HttpClientPostDto execute = null;
+            postList.stream().parallel().forEach(postId -> {
                 try {
-                    execute = (HttpClientPostDto) httpclient.execute(httpPost, handler.getThreadsRankingResponseHandler());
+                    log.info("Call Post Reply User List = {}", postId);
+                    String token = getLsdToken();
+                    HttpPost httpPost = new HttpPost(ThreadsRequestProperty.BASE_URL.getProperty());
+                    headerService.setThreadsRequestDefaultHeader(httpPost, token, null);
+                    UrlEncodedFormEntity entity = getUrlEncodedBody("postID", postId, token, DocId.GET_POST.getId());
+                    httpPost.setEntity(entity);
+                    log.info("Executing request = {} ", httpPost.getRequestLine());
+                    HttpClientPostDto execute = (HttpClientPostDto) httpclient.execute(httpPost, handler.getThreadsRankingResponseHandler());
+                    addUserAndParsingReplyUser(execute, username, userList);
                 } catch (IOException e) {
                     log.error("Error Not Found Post = {}", e.getMessage());
                     throw new ServiceLogicException(ErrorCode.INTERNAL_SERVER_ERROR);
+                } catch (InterruptedException e) {
+                    throw new ServiceLogicException(ErrorCode.INTERNAL_SERVER_ERROR);
                 }
-                log.info("Executing request = {} ", httpPost.getRequestLine());
-                addUserAndParsingReplyUser(execute, username, userList);
             });
             return userList;
 
@@ -216,13 +215,13 @@ public class ThreadsRankingService {
             String pageContents = "";
             StringBuilder contents = new StringBuilder();
 
-            URL url = new URL(ThreadsRequestProperty.INSTA_URL.getProperty()+username);
-            URLConnection con = (URLConnection)url.openConnection();
+            URL url = new URL(ThreadsRequestProperty.INSTA_URL.getProperty() + username);
+            URLConnection con = (URLConnection) url.openConnection();
             InputStreamReader reader = new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8);
 
             BufferedReader buff = new BufferedReader(reader);
 
-            while((pageContents = buff.readLine())!=null){
+            while ((pageContents = buff.readLine()) != null) {
                 contents.append(pageContents);
                 contents.append("\r\n");
             }
@@ -232,8 +231,8 @@ public class ThreadsRankingService {
             Pattern pattern = Pattern.compile(regex);
             Matcher matcher = pattern.matcher(body);
             if (matcher.find()) {
-                log.info("GroupCount Called Crawling = {}",matcher.groupCount());
-                log.info("UserID Called Crawling = {}",matcher.group(1));
+                log.info("GroupCount Called Crawling = {}", matcher.groupCount());
+                log.info("UserID Called Crawling = {}", matcher.group(1));
                 return matcher.group(1);
             } else {
                 log.error("### ERROR Not Found getUserIdCrawling ###");
@@ -253,7 +252,7 @@ public class ThreadsRankingService {
             headerService.setRapidApiDefaultHeader(httpGet, RAPID_API_KEY);
             HttpClientUsersDto execute = (HttpClientUsersDto) httpclient.execute(httpGet, handler.getThreadsRankingResponseHandler());
             log.info("Executing request RapidAPI = {} ", httpGet.getRequestLine());
-            log.info("UserID Called RapidAPI = {}",execute.getUserId());
+            log.info("UserID Called RapidAPI = {}", execute.getUserId());
             return UserDto.of(execute);
         } catch (ServiceLogicException se) {
             log.error("Error = {}", se.getErrorCode().getMessage());
@@ -263,6 +262,7 @@ public class ThreadsRankingService {
             return null;
         }
     }
+
     private String getUserIdByInstagramApi(String username) throws IOException {
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
             log.info("Call getUserIdByInstagramApi");
@@ -273,7 +273,7 @@ public class ThreadsRankingService {
             if (execute.getStatus().equals("ok")) {
                 JsonElement jsonElement = JsonParser.parseString(gson.toJson(execute.getData()));
                 String userId = jsonElement.getAsJsonObject().get("user").getAsJsonObject().get("id").getAsString();
-                log.info("UserID Called InstagramApi = {}",userId);
+                log.info("UserID Called InstagramApi = {}", userId);
                 return userId;
             } else {
                 throw new ServiceLogicException(ErrorCode.NOT_FOUND);
@@ -298,7 +298,7 @@ public class ThreadsRankingService {
         return idList;
     }
 
-    private List<String> parsingThreadsIdListByRapidApi(String  execute) {
+    private List<String> parsingThreadsIdListByRapidApi(String execute) {
         JsonElement el = JsonParser.parseString(execute);
         JsonArray array = el.getAsJsonObject().get("threads").getAsJsonArray();
         List<String> idList = new ArrayList<>();
